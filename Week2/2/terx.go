6ffc36323bf16a94ba9aaa610888b7f1892de44a
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-11-23 22:49:37 +0700

    Update terx.go

---
+++ Week2/2/terx.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	Number1 := 6240
+	Number2 := 7745
+	Result := Number1 + Number2
+	fmt.Printf("Number1 = %d Number2 = %d Result = %d", Number1, Number2, Result)
+}
