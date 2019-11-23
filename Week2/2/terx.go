74fac67413e91660963606277fbafe75c6f97dfb
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-11-24 02:56:13 +0700

    Update terx.go

--- Week2/2/terx.go
+++ Week2/2/terx.go
@@ -5,9 +5,14 @@ import "fmt"
 func main() {
 
 	fmt.Print(`\n \t Backticks`)
+
 	fmr.Print("\n \t double quote")
+
 	fmt.Println()
+
 	fmt.Println(`\n \t Backticks`[0])
-	fmt.Println(len(\n \t Backticks`))
+
+	fmt.Println(len(`\n \t Backticks`))
+
 	fmt.Println("Hello" + "World")
 }
