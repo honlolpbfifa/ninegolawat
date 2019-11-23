ec191971431492eb612718b9ab7df42c7942f2f7
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-11-24 02:38:27 +0700

    Update terx.go

--- Week2/2/terx.go
+++ Week2/2/terx.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	Number1 := 6240
-	Number2 := 7745
-	Result := Number1 + Number2
-	fmt.Printf("Number1 = %d Number2 = %d Result = %d", Number1, Number2, Result)
+
+	fmt.Print('\n \t backticks')
+	
+	
 }
