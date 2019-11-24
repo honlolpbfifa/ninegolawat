f94ba7eeaea5a2fc2428f02c056190e7a44eb32e
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-11-24 19:49:15 +0700

    Update terx.go

--- Week2/15/terx.go
+++ Week2/15/terx.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println(strings.trim(-Hello - World))
+	fmt.Println(strings.trim("-Hello - World-", "-"))
+	fmt.Println(strings.Trim("+Hello World-", "-+"))
 
 }
