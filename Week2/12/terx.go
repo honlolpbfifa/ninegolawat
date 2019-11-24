61f76e3c3f591ba53929b64e7d372a9ad9212925
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-11-24 18:04:40 +0700

    Update terx.go

--- Week2/12/terx.go
+++ Week2/12/terx.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println
+	fmt.Println(strings.Index("Hello World", "o"))
+	fmt.Println(strings.LastIndex("Hello World", "o"))
 
 }
