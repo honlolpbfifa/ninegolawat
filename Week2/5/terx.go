d15aabff28f01d9b8ebc0135b30d39aabea767bf
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-11-24 15:19:20 +0700

    Update terx.go

--- Week2/5/terx.go
+++ Week2/5/terx.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 	"strings"
 	"time"
-
 )
+
 func main() {
 	startA := Time.Now()
-	Var a string
+	var a string
 	for i := 0; i < 10000; i++ {
-		a +="a"
+		a += "a"
 	}
 	fmt.Println("a", time.since(startA))
 
 	startB := time.Now()
-	Var B strings.Builder
+	var b strings.Builder
 	for i := 0; i < 10000; i++ {
 		b.WritesString("B")
 	}
