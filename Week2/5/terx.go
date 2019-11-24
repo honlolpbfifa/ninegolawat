d214a1b933d9d04612c0ea20de832352cf7ecb65
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-11-24 15:12:24 +0700

    Update terx.go

--- Week2/5/terx.go
+++ Week2/5/terx.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+	"time"
 
+)
+func main() {
+	startA := Time.Now()
+	Var a string
+	for i := 0; i < 10000; i++ {
+		a +="a"
+	}
+	fmt.Println
 }
