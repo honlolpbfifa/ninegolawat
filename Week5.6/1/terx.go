193676d86336eaca94c95c053f6e67fc201e9411
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-07 21:46:57 +0700

    sss

---
+++ Week5.6/1/terx.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	student_1 := "Narutoooo"
+	student_2 := "Sasukaaa"
+	student_3 := "Sakuura"
+	student_4 := "pongmoung"
+	student_5 := "maremoung"
+	student_6 := "issa"
+	student_7 := "zaha"
+	student_8 := "paap"
+	student_9 := "gara"
+	student_10 := "hare"
+
+	fmt.Println(student_1, student_2, student_3, student_4, student_5, student_6, student_7, student_8, student_9, student_10)
+
+}
