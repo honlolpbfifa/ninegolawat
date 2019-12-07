ef429b5f92a31ca24614c3afd8d7db85c3d2e553
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-07 21:52:35 +0700

    Update terx.go

--- Week5.6/2/terx.go
+++ Week5.6/2/terx.go
@@ -1,5 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
 	student := []string{"goko", "goko1", "goko2", "goko3", "goko4", "goko5", "goko6", "goko7", "goko8", "goko9"}
+
+	fmt.Println(student)
 }
