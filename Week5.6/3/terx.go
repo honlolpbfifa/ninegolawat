95eac2155c5217d3a79c7525d282b108e9ec3ec2
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 16:23:20 +0700

    56

--- Week5.6/3/terx.go
+++ Week5.6/3/terx.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []string{"a, b, c, d, e"}
+	numbers := [1]string{"a, b, c, d, e"}
 	tornunsue := []int{1, 2, 3, 4}
 	B3 := []float64{3.2, 1.9, 1.235, 5.555555, 1.1}
 
---
+++ Week5.6/5/terx.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	B1 := [10]string{"A, s, d, f, g, h, j, k, l, m"}
+	fmt.println(B1)
+
+}
