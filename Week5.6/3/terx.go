1b4ebaac7a9f5000b778a641bd9f7281ade49725
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 15:14:56 +0700

    Update terx.go

--- Week5.6/3/terx.go
+++ Week5.6/3/terx.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var numbers [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	primes := [5]int{2, 3, 4, 5, 6}
+	numbers := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
 	fmt.Println(numbers)
-	fmt.Println(primes)
 
 }
