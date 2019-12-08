eedf115dd66d1ba5684fd8c88bb198e2a9995d6f
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 20:21:31 +0700

    w56

--- Week5.6/7/terx.go
+++ Week5.6/7/terx.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	N1 := [1][3]string{{"a", "b", "c"}, {"A", "B", "C"}}
+	N1 := [2][3]string{{"a", "b", "c"}, {"A", "B", "C"}}
 	fmt.Println(N1)
 	fmt.Println(N1[0][1])
 	Nm := [2][3][2]int{
---
+++ Week5.6/9/terx.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+func main() {
+	names := []string{}
+	names[0] := "Guko"
+	fmt.Println(names)
+	
+}
