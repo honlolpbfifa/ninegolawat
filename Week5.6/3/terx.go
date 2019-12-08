0cf1841d4408f11a958f554dab89112e5c319a61
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 15:40:33 +0700

    Update terx.go

--- Week5.6/3/terx.go
+++ Week5.6/3/terx.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	numbers := [5]string{"a, b, c, d, e"}
 	tornunsue := [5]int{1, 2, 3, 4}
-	B3 := [5]float64{3.2, 1.9, 1.235, 5.555555, 1.1}
+	B3 := []float64{3.2, 1.9, 1.235, 5.555555, 1.1}
 
 	fmt.Println(numbers)
 	fmt.Println(tornunsue)
