20552ec61ccfb9b033a900d08dc1e66ebd740035
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 16:14:28 +0700

    56

--- Week5.6/3/terx.go
+++ Week5.6/3/terx.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	numbers := [5]string{"a, b, c, d, e"}
+	numbers := [10]string{"a, b, c, d, e"}
 	tornunsue := [5]int{1, 2, 3, 4}
 	B3 := []float64{3.2, 1.9, 1.235, 5.555555, 1.1}
 
--- Week5.6/4/terx.go
+++ Week5.6/4/terx.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 	primes := [10]int{2, 4, 8, 16, 32, 64, 65}
-	B3 := [3]string{"Cat, Dog, Ratdoto"}
+	B3 := []string{"Cat, Dog, Ratdoto"}
 	fmt.Println(primes[6])
 	fmt.Println(primes[0])
-	fmt.Println(B3[1])
+	fmt.Println(B3)
 
 }
