bb78796694bb63a5d673215d19baf784b771f8fc
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 15:45:05 +0700

    Update terx.go

--- Week5.6/4/terx.go
+++ Week5.6/4/terx.go
@@ -4,8 +4,9 @@ import "fmt"
 
 func main() {
 	primes := [10]int{2, 4, 8, 16, 32, 64, 65}
-	B3 := [3]string{c, D, R}
+	B3 := [3]string{"Cat, Dog, Ratdoto"}
 	fmt.Println(primes[6])
 	fmt.Println(primes[0])
+	fmt.Println(B3[1])
 
 }
