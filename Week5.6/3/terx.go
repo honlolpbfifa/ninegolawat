3cb469f690704d090cbb31ac65d3989bf4975718
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 15:09:20 +0700

    Update terx.go

--- Week5.6/3/terx.go
+++ Week5.6/3/terx.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var alphabets [5]string{"ก, ข, ค, ง, จ, ฉ, ช, ซ, ง, ห, พ, ฟ, ผ, ฮ"}
-	fmt.Println( alphabets )
+	var numbers [15]int
+	fmt.Println(numbers)
 
 }
