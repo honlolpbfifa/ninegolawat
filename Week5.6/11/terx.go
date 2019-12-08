0bedad717184f606df21dcc1ff2ff40c07305cb1
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 21:02:39 +0700

    Update terx.go

--- Week5.6/11/terx.go
+++ Week5.6/11/terx.go
@@ -9,10 +9,13 @@ func main() {
 	x := B1[0:2]
 	fmt.Println(x)
 
-	y := B1[0:2]
+	y := B1[2:4]
 	fmt.Println(y)
 
-	z := B1[0:2]
+	z := B1[0:1]
 	fmt.Println(z)
+
+	z[0] = "X"
+	fmt.Println(B1)
 
 }
