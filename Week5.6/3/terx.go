d4bccdf462472244b7f6d7c8c5232cece654ef2f
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 15:18:37 +0700

    Update terx.go

--- Week5.6/3/terx.go
+++ Week5.6/3/terx.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	numbers := [1]string{"a, b, c, d, e"}[4]int{1, 2, 3, 4,}
+	numbers := [1]string{"a, b, c, d, e"}
+	tornunsue := [4]int{1, 2, 3, 4}
 
 	fmt.Println(numbers)
+	fmt.Println(tornunsue)
 
 }
