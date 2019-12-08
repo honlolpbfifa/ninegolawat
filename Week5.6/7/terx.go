28e71fb9d765936a5d990178da932407f9255e53
honlolpbfifa <57857956+honlolpbfifa@users.noreply.github.com>
2019-12-08 17:44:33 +0700

    Update terx.go

--- Week5.6/7/terx.go
+++ Week5.6/7/terx.go
@@ -6,5 +6,13 @@ func main() {
 	N1 := [2][3]string{{"a", "b", "c"}, {"A", "B", "C"}}
 	fmt.Println(N1)
 	fmt.Println(N1[0][1])
+	Nm := [2][3][2]int{
+		{
+			{1, 2}
+			{10, 20}
+			{100, 200}
+
+		}
+	}
 
 }
