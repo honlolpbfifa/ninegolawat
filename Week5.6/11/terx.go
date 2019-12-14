c6229bf6981dae45b87e5c533f490f2c66193cd0
honlolpbfifa <kopter2012@hotmail.com>
2019-12-14 20:34:38 +0700

    งง

--- Week5.6/11/terx.go
+++ Week5.6/11/terx.go
@@ -6,13 +6,13 @@ func main() {
 	B1 := [6]string{"A", "B", "C", "D", "E", "F"}
 	fmt.Println(B1)
 
-	x := B1[0:2]
+	x := B1[0:4]
 	fmt.Println(x)
 
 	y := x[2:4]
 	fmt.Println(y)
 
-	z := x[0:1]
+	z := y[0:1]
 	fmt.Println(z)
 
 	z[0] = "X"
---
+++ Week7/1/terx.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func say() {
+	fmt.Println("Hellojojo")
+
+}
