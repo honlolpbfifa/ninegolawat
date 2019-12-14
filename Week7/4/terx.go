e3619742efa624b23acfb1cf0545e943c174c809
honlolpbfifa <kopter2012@hotmail.com>
2019-12-14 21:01:19 +0700

    Update terx.go

--- Week7/4/terx.go
+++ Week7/4/terx.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func sun(numbers ...int) int {
 	total := 0
 	for _, n := range numbers {
@@ -8,5 +10,7 @@ func sun(numbers ...int) int {
 	return total
 }
 func main() {
+	x := sun(1, 3, 5, 7, 9)
+	fmt.Println
 
 }
