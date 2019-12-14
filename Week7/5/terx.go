9acec76a5c5a4efb37a3caab1f7bf0a6657fdd4e
honlolpbfifa <kopter2012@hotmail.com>
2019-12-14 21:07:47 +0700

    Create terx.go

---
+++ Week7/5/terx.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func sum(numbers ...int) int {
+	total := 0
+	for _, n := range numbers {
+		total = total + n
+	}
+	return total
+}
+func main() {
+	number := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	x := sum(number...)
+	fmt.Println(x)
+}
