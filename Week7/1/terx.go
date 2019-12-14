3c30eeafb3a20c6aebc7b02fdfbd9f9b375311c3
honlolpbfifa <kopter2012@hotmail.com>
2019-12-14 20:44:14 +0700

    Update terx.go

--- Week7/1/terx.go
+++ Week7/1/terx.go
@@ -2,12 +2,6 @@ package main
 
 import "fmt"
 
-<<<<<<< HEAD
 func main() {
-	fmt.Println("Hello")
-=======
-func say() {
-	fmt.Println("Hellojojo")
-
->>>>>>> 43bdbd7e22e46d6ee5641dc4dd8b5c246dd6c270
+	fmt.Println("Hessssssssllo")
 }
