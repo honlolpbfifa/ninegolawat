503f6cdfc77dd5f608fcf96e0b908dde6646112b
honlolpbfifa <kopter2012@hotmail.com>
2019-12-14 20:47:09 +0700

    Update terx.go

--- Week7/1/terx.go
+++ Week7/1/terx.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func say() {
+	fmt.Println("Hello")
+}
+
+func greet(name string) {
+	fmt.Println("Hello", name)
+}
+
 func main() {
-	fmt.Println("Hessssssssllo")
+	say()
+	greet("terx")
 }
