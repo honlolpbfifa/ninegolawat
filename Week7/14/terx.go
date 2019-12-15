f22790e1e0c84c2313a61084381e95e9ce2e00fa
honlolpbfifa <kopter2012@hotmail.com>
2019-12-15 15:09:34 +0700

    Update terx.go

---
+++ Week7/14/terx.go
@@ -0,0 +1,14 @@
+package main
+
+func compute(fn func(int, int) int) int {
+	return fn(3, 4)
+}
+func main() {
+	sum := func(x, y int) int {
+		return x + y
+	}
+	subtract := func(x, y int) int {
+		return x - y
+	}
+
+}
