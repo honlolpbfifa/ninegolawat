b622899639b3ccbbe9bc1a4c05e49c7c65135f65
honlolpbfifa <kopter2012@hotmail.com>
2020-01-26 18:28:25 +0700

    Create terx.go

---
+++ Week 10/4/terx.go
@@ -0,0 +1,14 @@
+package main
+
+import "os"
+
+func main() {
+	file, err := os.Create("myFile.txt")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	file.WriteStrinf("Hello \n")
+	file.WriteStrinf
+}
