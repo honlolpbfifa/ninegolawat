c117d0c452678fb2369d9cdd1bd7943223181b81
honlolpbfifa <kopter2012@hotmail.com>
2020-01-26 15:31:46 +0700

    Update terx.go

--- Week 10/1/terx.go
+++ Week 10/1/terx.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"io"
+)
+
 func main() {
 	reader := string.NewReader("HelloWorld")
 	P := make([]byte, 3)
-	for
+	for {
+		n, err := reader.Read(p)
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(string(p[:n]))
+	}
 }
