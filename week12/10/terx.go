d514fbcb2d28458dc41778d00b8fde39765105cd
honlolpbfifa <kopter2012@hotmail.com>
2020-02-02 20:32:52 +0700

    ss

---
+++ week12/10/terx.go
@@ -0,0 +1,14 @@
+package main
+func printer(c chan int, number ...int) {
+	sum := 0
+	for _, V := range number {
+		sum = sum + v
+	}
+	func printer(c chan int) {
+		number := <-c
+		fmt.Println(number)
+	}
+}
+func main() {
+	
+}
