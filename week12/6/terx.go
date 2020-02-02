b63d29a1f514d54ba769e0d23df904c6205237f0
honlolpbfifa <kopter2012@hotmail.com>
2020-02-02 19:56:42 +0700

    Update terx.go

--- week12/6/terx.go
+++ week12/6/terx.go
@@ -1,10 +1,20 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func increment(data *int, mutex *sync.WaitGroup) {
-
+	start := time.Now()
+	defer wg.Done()
+	defer mutrx.Unlock()
+	mutex.Lock()
+	*data++
+	fmt.Println(time.Since(start), "Increment to:", *data)
 }
+func read(data)
 func main() {
 
 }
