0a3ce09a7d3a982a50cdd567c5e516cf8f11de3c
honlolpbfifa <kopter2012@hotmail.com>
2020-02-02 20:38:33 +0700

    Update terx.go

--- week12/10/terx.go
+++ week12/10/terx.go
@@ -1,14 +1,25 @@
 package main
-func printer(c chan int, number ...int) {
+
+import "fmt"
+
+func sum(c chan int, number ...int) {
 	sum := 0
 	for _, V := range number {
 		sum = sum + v
 	}
-	func printer(c chan int) {
-		number := <-c
-		fmt.Println(number)
-	}
+	c <- sum
+}
+func printer(c chan int) {
+	number := <-c
+	fmt.Println(number)
 }
 func main() {
-	
+	c := make(chan int)
+	go printer(c)
+	go printer(c)
+	go sum(c, 1, 2, 3)
+	go sum(c, 10, 11)
+
+	vat input string
+
 }
