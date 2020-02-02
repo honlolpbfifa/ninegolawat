9dbfecf5cb04633c79cca1d2bd8ac5a0f2b26802
honlolpbfifa <kopter2012@hotmail.com>
2020-02-02 19:14:18 +0700

    ;;

---
+++ Week 11/2/terx.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileSize := stat.Size()
+	p := make([]byte, fileSize)
+	file.Read(p)
+
+	str := string(p)
+	fmt.Println(str)
+}
